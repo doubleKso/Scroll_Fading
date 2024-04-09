218685c7df70c125033e9f30b5f139f5f4379bab
DoubleKSO <kaungkhantsheinoo98.ucsm@gmail.com>
2024-04-09 19:33:57 +0630

    scroll_fading

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:fade_scrolling/config/theme.dart';
+import 'package:fade_scrolling/views/final_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: const FinalView(),
+    );
+  }
+}
